e2ed39856cf25e48218ed9c03b66e83f5e58c956
Rigel-Villanueva <alonsovillanueva302006@gmail.com>
2026-02-24 10:59:37 -0600

    Fix Swagger configuration and resolve ReflectionTypeLoadException in .NET 9

--- Program.cs
+++ Program.cs
@@ -11,15 +11,16 @@ builder.Services.Configure<KioskoDatabaseSettings>(
 builder.Services.AddSingleton<ProyectosService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
